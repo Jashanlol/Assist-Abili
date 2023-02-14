ae735bf8142a833ff152f8b485e3e02b8d1d89ae
Raghav <rbatra2@scu.edu>
2023-02-13 16:16:58 -0800

    almost got jest to work

---
+++ js/darkmode.test.js
@@ -0,0 +1,21 @@
+const { getComputedStyle } = require('@testing-library/jest-dom');
+
+test('Styles are applied correctly', () => {
+  // Load the HTML file into an iframe
+  const iframe = document.createElement('iframe');
+  iframe.src = 'html/popup.html';
+  document.body.appendChild(iframe);
+
+  // Get the elements in the iframe
+  const slidern1 = iframe.contentDocument.querySelector('.options input:nth-of-type(1).checked ~ .slider .sliderknob');
+  const slidern2 = iframe.contentDocument.querySelector('.options input:nth-of-type(2).checked ~ .slider .sliderknob');
+
+  // Check the styles of the n1 element
+
+  expect(getComputedStyle(slidern1).backgroundColor).toBe('rgb(141, 210, 141)');
+
+
+  // Check the styles of the n2 element
+  expect(getComputedStyle(slidern2).backgroundColor).toBe('rgb(114, 114, 114)');
+  expect(getComputedStyle(slidern2).transform).toBe('-30px');
+});
