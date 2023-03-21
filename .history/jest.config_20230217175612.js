898ced2f8804e156d6c8f28a527fc52c1dc9c237
Jashan <37347402+Jashanlol@users.noreply.github.com>
2023-03-21 01:15:28 -0700

    Delete .history directory

--- .history/jest.config_20230217175612.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    testTimeout: 5000,
-    // setupFilesAfterEnv: ['./test-setup.js'],
-    testEnvironment: 'jest-environment-jsdom',
-    verbose:true,
-  };
-
--- .history/jest.config_20230220184046.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    testTimeout: 5000,
-    //setupFilesAfterEnv: ['./test-setup.js'],
-    testEnvironment: 'jest-environment-jsdom',
-    verbose:true,
-    modulePaths: '<rootDir>'
-  };
-
